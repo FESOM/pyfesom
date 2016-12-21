2f50c03c2ec713fd24c2861de1487ae8979d3fbc
Nikolay Koldunov <koldunovn@gmail.com>
2016-12-21 17:34:57 +0100

    Update __init__.py
    Change header in the init file

--- pyfesom/__init__.py
+++ pyfesom/__init__.py
@@ -1,13 +1,11 @@
-"""
-################################################################################
-#
-# PyFESOM - tools for the FESOM ocean model.
+# pyfesom
+# -------
+# python tools for FESOM ocean model
 #
 # Original code by Dmitry Sidorenko, 2013
 #
-################################################################################
+# Copyright (c) 2016 FESOM team
 
-"""
 from .load_mesh_data import *
 from .regriding import *
 from .woa2005 import *
