ba4fad32081dc0997f3bca00a026a83ea833d997
Nikolay Koldunov <koldunovn@gmail.com>
2016-12-22 10:35:40 +0100

    change to python3 way pickle

--- pyfesom/__init__.py
+++ pyfesom/__init__.py
@@ -6,11 +6,28 @@
 #
 # Copyright (c) 2016 FESOM team
 
+import sys
+
+if sys.version_info < (3,3):
+    raise ImportError(
+"""
+pyfesom does not support Python 2.6, 2.7, 3.0, 3.1, or 3.2.
+""")
+
 from .load_mesh_data import *
 from .regriding import *
 from .woa2005 import *
 from .ut import *
 from .climatology import *
+
+
+__author__   = 'FESOM team'
+__license__  = 'MIT'
+__version__  = '0.1'
+
+
+
+
 
 
 
