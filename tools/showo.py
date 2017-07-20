12ab24c8cae4e4642e799806b1f6659be4f30ed5
Nikolay Koldunov <koldunovn@gmail.com>
2017-07-20 15:48:02 +0200

    add display of information

--- tools/showo.py
+++ tools/showo.py
@@ -15,40 +15,6 @@ import pyfesom as pf
 from cartopy.util import add_cyclic_point
 
 
-# @click.argument('ifile', type=click.Path(exists=True))
-# @click.argument('variable', default='temp', required=False)
-# @click.option('--depth', '-d', default=0, type=click.FLOAT, show_default=True,
-#                help='Depth in meters.')
-# @click.option('--box', '-b',
-#               nargs=4,
-#               type=(click.IntRange(-180, 180),
-#                     click.IntRange(-180, 180),
-#                     click.IntRange(-90, 90),
-#                     click.IntRange(-90, 90)),
-#              default=(-180,180,-80,90), show_default=True,
-#              help='Map boundaries in -180 180 -90 90 format.')
-# @click.option('--res', '-r', nargs=2,
-#               type=(click.INT, click.INT),
-#               default=(360, 170), show_default=True,
-#               help='Number of points along each axis (for lon and  lat).')
-# @click.option('--influence','-i', default=80000, show_default=True,
-#               help='Radius of influence for interpolation, in meters.')
-# @click.option('--timestep', '-t', default=0, show_default=True,
-#               help='Timstep from netCDF variable, strats with 0.')
-# @click.option('--levels', '-l', nargs=3, type=click.INT,
-#               help='Levels for contour plot in format min max numberOfLevels.\
-#  If not provided min/max values from data will be used with 40 levels.')
-# @click.option('--quiet', '-q', is_flag=True,
-#               help='If present additional information will not be printed.')
-# @click.option('--ofile','-o' ,type=click.Path(exists=False) ,
-#              help='Path to the output figure. If present the image\
-#  will be saved to the file instead of showing it. ')
-# @click.option('--mapproj','-m', type=click.Choice(['merc', 'pc','np','sp', 'rob']),
-#               default='merc')
-# def showfile(ifile, variable, depth,
-#              meshpath, box, res, influence,
-#              timestep, levels, quiet, ofile, mapproj):
-#              pass
 @click.command()
 @click.argument('meshpath', type=click.Path(exists=True))
 @click.argument('ifile', type=click.Path(exists=True))
@@ -69,8 +35,33 @@ from cartopy.util import add_cyclic_point
               help='Minimun and maximum values for plotting.')
 @click.option('--mapproj','-m', type=click.Choice(['merc', 'pc','np','sp', 'rob']),
                default='merc')
+@click.option('--quiet', '-q', is_flag=True,
+              help='If present additional information will not be printed.')
+@click.option('--ofile','-o' ,type=click.Path(exists=False) ,
+              help='Path to the output figure. If present the image\
+              will be saved to the file instead of showing it. ')
 def showo(meshpath, ifile, variable, 
-          depth, box, timestep, minmax, mapproj):
+          depth, box, timestep, minmax, 
+          mapproj, quiet, ofile):
+    '''
+    meshpath - Path to the folder with FESOM1.4 mesh files.
+
+    ifile    - Path to FESOM1.4 netCDF file.
+
+    variable - The netCDF variable to be plotted.
+    '''
+    if not quiet:
+        click.secho('Mesh: {}'.format(meshpath))
+        click.secho('File: {}'.format(ifile))
+        click.secho('Variable: {}'.format(variable), fg='red')
+        click.secho('Depth: {}'.format(depth), fg='red')
+        click.secho('BOX: {}'.format(box))
+        click.secho('Timestep: {}'.format(timestep))
+        if minmax:
+            click.secho('Min/max: {}'.format(minmax), fg='red')
+        else:
+            click.secho('Min/max: auto', fg='red')
+
     mesh = pf.load_mesh(meshpath)
     flf = Dataset(ifile)
     left, right, down, up = box
