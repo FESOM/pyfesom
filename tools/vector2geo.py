806a62fd4966dc74e1ede3c21f4e57399b762cc2
Nikolay Koldunov <koldunovn@gmail.com>
2018-02-05 08:45:49 +0100

    fix 2d case for vector2geo

--- tools/vector2geo.py
+++ tools/vector2geo.py
@@ -169,6 +169,15 @@ def vector2geo(ifile, opath, variable,
     ofile = os.path.join(opath, '{}_{}.nc'.format(os.path.basename(ifile)[:-3], ext))
 
     fl = Dataset(ifile)
+    if fl.variables[variable].shape[1] == mesh.n2d:
+        dim3d = False
+        dind = [dind[0]]
+        realdepth = [realdepth[0]]
+    elif fl.variables[variable].shape[1] == mesh.n3d:
+        dim3d = True
+    else:
+        raise ValueError('Variable size {} is not equal to number of 2d ({}) or 3d ({}) nodes'.format(fl.variables[variable].shape[1], mesh.n2d, mesh.n3d))
+
     fw = Dataset(ofile, mode='w',data_model='NETCDF4_CLASSIC', )
 
     fw.createDimension('latitude', lonreg2.shape[0])
@@ -197,12 +206,7 @@ def vector2geo(ifile, opath, variable,
         time[:] = fl.variables['time'][timestep]
     print(time.shape)
 
-    if fl.variables[variable].shape[1] == mesh.n2d:
-        dim3d = False
-    elif fl.variables[variable].shape[1] == mesh.n3d:
-        dim3d = True
-    else:
-        raise ValueError('Variable size {} is not equal to number of 2d ({}) or 3d ({}) nodes'.format(fl.variables[variable].shape[1], mesh.n2d, mesh.n3d))
+
 
     # ii = LinearNDInterpolator(qh, mesh.topo)
     if timestep == -1:
