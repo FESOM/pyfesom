247de7b29f48dedd1cd7c4a61df0a8ca40933dad
Jan Streffing <jan.streffing@awi.de>
2019-10-15 13:13:38 +0200

    added parameter to climatology that allows for seasonal or monthly cl… (#10)
    * added parameter to climatology that allows for seasonal or monthly clim. Also added en4 as one example of such
    
    * Provide default value for record and some doc

--- pyfesom/climatology.py
+++ pyfesom/climatology.py
@@ -1,6 +1,6 @@
 # This file is part of pyfesom
 #
-################################################################################
+#################################################################################
 #
 # Original code by Dmitry Sidorenko, 2013
 #
@@ -8,9 +8,10 @@
 #   Nikolay Koldunov, 2016
 #          - change to netCDF4 
 #          - change scipy griddata interpolation to KDTree for speed
-# TODO
-# Add seasonal climatology
-################################################################################
+#   Jan Streffing, 2019
+#          - added record parameter to process seasonal and monthly climatologies
+#          - added en4 monthly climatology
+#################################################################################
 
 import numpy as np
 import scipy as sc
@@ -29,7 +30,11 @@ class climatology(object):
     path : str
         Path to the directory with climatology files
     climname : str
-        Name of the climatology ('woa05' or 'phc')
+        Name of the climatology ('woa05', 'phc', 'gdem', 'en4')
+    record: int
+        Number of record in climatology file. e.g.
+	0 to 3 for seasonal, 0 to 11 for monthly. Only supported
+	for `en4` at the moment
 
     Returns
     -------
@@ -53,7 +58,7 @@ class climatology(object):
         zonal mean of salinity
 
     '''
-    def __init__(self, path, climname='woa05'):
+    def __init__(self, path, climname='woa05', record=0):
         if climname=='woa05':
             ncfile = Dataset(os.path.join(path, 'woa2005TS.nc'))
             self.T = np.copy(ncfile.variables['t00an1'][0,:,:,:])
@@ -123,5 +128,27 @@ class climatology(object):
             self.Syz=nanmean(self.S, 2)
             self.T = np.ma.masked_less(self.T,-1000)
             self.S = np.ma.masked_less(self.S,-1000)
-
+        if climname=='en4':
+            ncfile = Dataset(os.path.join(path, 'en4_monthly.nc'))
+            self.T = np.copy(ncfile.variables['temperature'][record,:,:,:])
+            x=np.copy(ncfile.variables['lon'][:])
+            x[x>180]=x[x>180]-360
+            ind=[i[0] for i in sorted(enumerate(x), key=lambda x:x[1])]
+            x=np.sort(x)
+            self.x=x
+            self.y=ncfile.variables['lat'][:]
+            self.z=ncfile.variables['depth'][:]
+            self.T[:,:,:]=self.T[:,:,ind]-273.15
+            self.S=np.copy(ncfile.variables['salinity'][record,:,:,:])
+            self.S[:,:,:]=self.S[:,:,ind]
+            depth3d = np.zeros(self.S.shape)
+            for i in range(self.z.shape[0]):
+                depth3d[i, :, :] = self.z[i]
+            ptemp = sw.ptmp(self.S, self.T, depth3d)
+            self.T = ptemp
+            ncfile.close()
+            self.Tyz=nanmean(self.T, 2)
+            self.Syz=nanmean(self.S, 2)
+            self.T = np.ma.masked_less(self.T,-1000)
+            self.S = np.ma.masked_less(self.S,-1000)
 
